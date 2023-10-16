408cf776f4b573a9e5dd12763381ce5ec055311b
22mahmoud <hello@mahmoudashraf.dev>
2023-10-16 12:04:28 +0300

    remove api

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default async function handler(
-  _req: VercelRequest,
-  res: VercelResponse
-) {
-  res.status(200).json({ message: "Hello World" });
-}
